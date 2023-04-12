b8d11d40314c97da818402506ed33eccb9df10b7
NancyD2017 <aa.alifanova@spbstu.ru>
2023-04-12 22:37:29 +0300

    d

---
+++ src/main/java/org/example/Inventory.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+import java.util.Map;
+
+public class Inventory {
+    class Player {
+        String color;
+        Map<Integer, Sprite> available;
+        Integer points;
+        List<Sprite> ports;
+    }
+}
