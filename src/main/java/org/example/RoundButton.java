07f7108ef1d9e39fe77aabb8b8a3de1682180ce0
NancyD2017 <aa.alifanova@spbstu.ru>
2024-11-23 17:16:36 +0300

    buttons even road and circle positioned correctly, angled (not positioned right) and circles with background

--- src/main/java/org/example/RoundButton.java
+++ src/main/java/org/example/RoundButton.java
@@ -11,42 +11,23 @@ public class RoundButton extends JButton implements ActionListener {
 
     public RoundButton(String text, int x, int y) {
         super();
-        setBounds(x, y, 70, 70);
+        setBounds(x, y, 50, 50);
         setActionCommand(text);
         addActionListener(this);
-        setOpaque(false);
         setContentAreaFilled(false);
-        setBorderPainted(false);
-        setOpaque(false);
-        setFocusPainted(false);
+        setBackground(new Color(0, 255,0,55));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
-
-        // Включаем антиалиасинг
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Если кнопка нажата, меняем цвет
-        if (getModel().isArmed()) {
-            g2d.setColor(Color.LIGHT_GRAY);
-        } else {
-            g2d.setColor(getBackground());
-        }
-
-        // Рисуем круг
+        g2d.setColor(Color.LIGHT_GRAY);
         g2d.fillOval(0, 0, getWidth() - 1, getHeight() - 1);
-
         g2d.dispose();
-
-        // Убираем любые дополнительные отрисовки кнопки
-        super.paintComponent(g);
     }
 
     protected void paintBorder(Graphics g) {
         g.setColor(getForeground());
-        g.drawOval(0, 0, getWidth() - 1, getHeight() - 1);
     }
     public void actionPerformed(ActionEvent e) {
         String[] position;
